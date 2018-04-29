88de1ac6e9ee12cd2dab10b178095c01872968e9
David <davidlang@gmx.com>
2018-04-29 18:48:05 -0500

    Initial Commit

---
+++ FlareFit macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  FlareFit macOS
+//
+//  Created by David Lang on 4/29/18.
+//  Copyright © 2018 David Lang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
